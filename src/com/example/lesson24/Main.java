d555d6b586fb85df02012924545777993cf890de
Yevhen Holienishchev <Yevhen Holienishchev>
2016-06-20 11:46:57 +0300

    reupload

--- src/com/example/lesson24/Main.java
+++ src/com/example/lesson24/Main.java
@@ -1,4 +1,21 @@
 package com.example.lesson24;
 
+//import java.lang.Thread;
 public class Main {
+    public static void main(String[] args) {
+        Thread thread = Thread.currentThread();
+        MessageLoop myMessageLoop = new MessageLoop();
+        System.out.println("Старт MessageLoop thread");
+        myMessageLoop.run();
+        while (thread.isAlive()) {
+            try {
+                Thread.sleep(1000);
+
+            } catch (InterruptedException e) {
+                System.out.println("InterruptedException");
+            }
+            //myMessageLoop.join();
+            System.out.println(thread.getName() + ": " + "Я жду...");
+        }
+    }
 }
--- src/com/example/lesson24/MessageLoop.java
+++ src/com/example/lesson24/MessageLoop.java
@@ -1,4 +1,21 @@
 package com.example.lesson24;
 
-public class MessageLoop {
+public class MessageLoop implements Runnable {
+    Thread thread;
+
+    private String[] messagesArray = {"Первая строка из массива,", "Вторая строка из массива,",
+            "Третья строка из массива,"};
+
+    public void run() {
+        thread = new Thread (this, "Thread-0");
+        for (int i = 0; i < messagesArray.length; i++) {
+            try {
+                Thread.sleep(4000);
+            } catch ( InterruptedException e){
+
+            }
+            System.out.println(thread.getName() +": " + messagesArray[i]);
+        }
+    }
 }
+ 
